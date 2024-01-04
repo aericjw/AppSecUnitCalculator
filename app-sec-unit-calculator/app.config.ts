1f47f929256c90c83b3f2228b33c0534744cd85f
aericjw <aericwalls26@gmail.com>
2024-01-04 11:54:57 -0700

    generalized app.config.ts

--- app-sec-unit-calculator/app.config.ts
+++ app-sec-unit-calculator/app.config.ts
@@ -1,10 +1,10 @@
 import type { CliOptions } from 'dt-app';
 
 const config: CliOptions = {
-  environmentUrl: 'https://xle26845.sprint.apps.dynatracelabs.com/',
+  environmentUrl: 'https://<environmentID>.apps.dynatrace.com/',
   app: {
     name: 'AppSec Unit Calculator',
-    version: '1.0.0',
+    version: '1.1.0',
     description: 'A simple projection calculator to estimate ASU consumption',
     id: 'my.app.sec.unit.calculator',
     scopes: [{ name: 'environment-api:entities:read', comment: 'Read Entities'}]
